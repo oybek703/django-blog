59ca0cc9d12855e0e71edee7a5e79660c0d178fe
oybek703 <hhoybek@gmail.com>
2021-12-02 14:58:55 +0500

    starting adding models

---
+++ blog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('posts/', views.posts, name='posts'),
+    path('posts/<slug>', views.post_details, name='post-details')
+]
