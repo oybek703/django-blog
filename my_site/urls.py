641e404f073ee6883b36ed6c73553e20d4e38137
oybek703 <hhoybek@gmail.com>
2021-12-04 03:23:21 +0500

    fail with aws elastic beanstalk deployment

--- my_site/urls.py
+++ my_site/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('', include('blog.urls')),
     path('admin/', admin.site.urls)
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
+  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
