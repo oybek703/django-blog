2121b72e9e3d9e81d17283b57ffca3e0e77f2140
oybek703 <hhoybek@gmail.com>
2021-12-04 14:49:46 +0500

    blog app deployed to aws elastic beanstalk

--- my_site/settings.py
+++ my_site/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('IS_DEVELOPMENT', True)
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'my_site/static')
 ]
