f99dc2db40bf1ea3c4482cadf4edac23406cdd40
oybek703 <hhoybek@gmail.com>
2021-12-04 16:28:30 +0500

    postgres database created in aws RDS and successfully connected

--- my_site/settings.py
+++ my_site/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'test123456',
+        'HOST': 'database-2.chscw1xkpvhl.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
