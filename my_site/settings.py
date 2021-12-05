8590c3632b5eb647ad12400e6df63876f57bd157
oybek703 <hhoybek@gmail.com>
2021-12-05 16:50:18 +0500

    static files are served from aws s3 bucket now

--- my_site/settings.py
+++ my_site/settings.py
@@ -156,7 +156,13 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_FOLDER = 'static'
+MEDIAFILES_FOLDER = 'media'
+
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'custom_storages.StaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaFilesStorage'
 
 
 
